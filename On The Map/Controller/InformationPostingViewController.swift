313b4a60e3ff6f870d2775e042de5d7b77e43ca0
Jimmy Gutierrez <jimmyg@Jimmys-MacBook-Pro.local>
2021-03-14 15:41:20 -0400

    added bar button items and information posting view controller + swift file, connected new view controllers via segue from plus sign button on navigation bar

---
+++ On The Map/Controller/InformationPostingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  InformationPostingViewController.swift
+//  On The Map
+//
+//  Created by Jimmy Gutierrez on 3/14/21.
+//
+
+import UIKit
+
+class InformationPostingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
